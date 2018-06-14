d69413d9449318a9a79d52f1e4fc68ae4a8dfd60
wrzfeijianshen <wrzfeijianshen@126.com>
2018-06-14 15:25:40 +0800

    2

--- docker/02_mysql/01_centos_mysql/downPack.sh
+++ docker/02_mysql/01_centos_mysql/downPack.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-NGINX_VERSION=1.15.0
-PCRE_VERSION=8.42
-ZLIB_VERSION=1.2.11
+#NGINX_VERSION=1.15.0
+#PCRE_VERSION=8.42
+#ZLIB_VERSION=1.2.11
 
 curl -o ./Centos-6.repo http://mirrors.aliyun.com/repo/Centos-6.repo 
 curl -o ./epel-6.repo http://mirrors.aliyun.com/repo/epel-6.repo 
-curl -o ./openssl-$OPENSSL_VERSION.tar.gz https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz 
-curl -o ./nginx-$NGINX_VERSION.tar.gz https://nginx.org/download/nginx-$NGINX_VERSION.tar.gz 
-curl -o ./pcre-$PCRE_VERSION.tar.gz https://ftp.pcre.org/pub/pcre/pcre-$PCRE_VERSION.tar.gz
+#curl -o ./openssl-$OPENSSL_VERSION.tar.gz https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz 
+#curl -o ./nginx-$NGINX_VERSION.tar.gz https://nginx.org/download/nginx-$NGINX_VERSION.tar.gz 
+#curl -o ./pcre-$PCRE_VERSION.tar.gz https://ftp.pcre.org/pub/pcre/pcre-$PCRE_VERSION.tar.gz
 
 #curl -o $BUILD_ROOT/zlib-$ZLIB_VERSION.tar.gz https://www.zlib.net/zlib-$ZLIB_VERSION.tar.gz
 #curl -o $BUILD_ROOT/zlib-$ZLIB_VERSION.tar.gz https://www.zlib.net/zlib-$ZLIB_VERSION.tar.gz
