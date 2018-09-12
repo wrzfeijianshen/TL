c6c9b1ae0975d9f5a313a63553e8fec63bdd2441
wrzfeijianshen <wrzfeijianshen@126.com>
2018-09-13 00:11:12 +0800

    add

--- c++/qt/code/chap01_Introduction/1_1_Hello/1_1_Hello.pro
+++ c++/qt/code/chap01_Introduction/1_1_Hello/1_1_Hello.pro
@@ -3,7 +3,7 @@ greaterThan(QT_MAJOR_VERSION,4) : QT += widgets
 TARGET = 1_1Hello
 TEMPLATE = app
 DEFINES += QT_DEPRECATED_WARNINGS
-
+#CONFIG +=console
 FORMS += \
     mainwindow.ui
 
--- c++/qt/code/chap01_Introduction/1_1_Hello/main.cpp
+++ c++/qt/code/chap01_Introduction/1_1_Hello/main.cpp
@@ -1,9 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <iostream>
+using namespace std;
+#include <QDebug>
 int main(int argc,char *argv[])
 {
     QApplication a(argc,argv);
+    //system("chcp 65001");
+    qDebug()<<"hello world!中文sss";
+    printf("%s\n","中文歌n");
+    cout << "中文aaaa";
     MainWindow w;
     w.show();
 
