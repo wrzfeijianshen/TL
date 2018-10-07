46db87cc44d4dc7d6fd69e143fc2d922ce180def
wrzfeijianshen <wrzfeijianshen@126.com>
2018-10-08 07:50:33 +0800

    aaa

--- golang/goqt/_1/1.5/02/ui/ui.go
+++ golang/goqt/_1/1.5/02/ui/ui.go
@@ -14,7 +14,6 @@ type Ui_MainWindow struct {
 	Centralwidget *qtwidgets.QWidget
 	MainWindow    *qtwidgets.QMainWindow
 	Label         *qtwidgets.QLabel
-	Dialog        *qtwidgets.QDialog
 }
 
 func NewUi_MainWindow() *Ui_MainWindow {
@@ -34,27 +33,20 @@ func (this *Ui_MainWindow) SetupUi(MainWindow *qtwidgets.QMainWindow) {
 
 	this.Label = qtwidgets.NewQLabel(this.Centralwidget, 0) // 111
 	this.Label.SetObjectName("Label")                       // 112
-	// this.Label.SetContentsMargins(50, 50, 100, 100)
-
 }
 func (this *Ui_MainWindow) RetranslateUi(MainWindow *qtwidgets.QMainWindow) {
 	MainWindow.SetWindowTitle("MainWindow")
-	// MainWindow.SetFixedSize_1(500, 500)
-	MainWindow.Resize(500, 500)
+	MainWindow.Resize(200, 200)
 	MainWindow.Move(0, 0)
 
-	// this.Centralwidget.SetFixedSize_1(500,500)
+	this.Centralwidget.Resize(200, 200)
 	this.Label.SetText("hello哈哈")
-
-	// this.Label.SetContentsMargins(100, 100, 100, 100)
-	// this.Label.SetFixedSize_1(100, 100)
-	// this.Label.Move(20, 20)
 
 	this.Label.SetGeometry(10, 10, 80, 80)
 }
+
 func NewUi_MainWindow2() *Ui_MainWindow {
 	this := &Ui_MainWindow{}
-	// w := qtwidgets.NewQMainWindow(nil, 0) // 创建QMainWindow
 	w := qtwidgets.NewQMainWindow(nil, 0)
 	this.SetupUi(w)                     // 设置控件UI，名称
 	this.RetranslateUi(this.MainWindow) // 设置控件属性
@@ -63,6 +55,7 @@ func NewUi_MainWindow2() *Ui_MainWindow {
 
 	return this
 }
+
 func (this *Ui_MainWindow) Show() {
 	this.MainWindow.Show()
 }
